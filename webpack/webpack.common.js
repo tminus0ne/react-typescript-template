504e2e8debf3727311f6527d44d57423a736b0da
Greg-One <sgmdoc@yandex.ru>
2021-12-14 22:09:36 +0500

    add babel-runtime & copy-webpack plugins

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -38,6 +39,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src',
+          to: 'build',
+        },
+      ],
     }),
   ],
 };
